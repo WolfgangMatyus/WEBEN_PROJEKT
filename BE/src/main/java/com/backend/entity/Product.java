94fe336771040c222aecfb1b270330e7466473e8
Matyus Wolfgang <wi21b055@technikum-wien.at>
2023-05-28 20:05:14 +0200

    Product Entry Points and Variables

--- BE/src/main/java/com/backend/entity/Product.java
+++ BE/src/main/java/com/backend/entity/Product.java
@@ -1,4 +1,28 @@
 package com.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.lang.model.element.NestingKind;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_product")
 public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private int price;
+    private String imagepath;
 }
